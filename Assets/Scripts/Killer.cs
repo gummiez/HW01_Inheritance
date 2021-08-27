de01ec91cf962748b4745cda19fb524a74ff7326
Tyty Cao <yaboyteets@gmail.com>
2021-08-26 19:30:45 -0500

    Created Killer and Killer script

---
+++ Assets/Scripts/Killer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killer : Enemy
+{
+	protected override void PlayerImpact(Player player)
+	{
+		player.Kill();
+	}
+}
